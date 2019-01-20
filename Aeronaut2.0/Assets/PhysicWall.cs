84de0c205b90f180abdf1ced6ad847166bf287e4
piLeoni <pietro.leoni@gmail.com>
2019-01-20 13:40:36 -0500

    unstaged big files
    Former-commit-id: 78014a076963babe0874e15d472c4d53b63b815a

---
+++ Aeronaut2.0/Assets/PhysicWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicWall : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /*
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject varGameObject = GameObject.Find("[CameraRig]");
+        if (otherObject.gameObject.tag == "[CameraRig]")
+        {
+        transform.Translate(Vector3(0,0,0));
+        shouldMove = false;
+}
+*/
+
+}
